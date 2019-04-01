605630449449db15633621fd18df1e03e7f7d430
J Khalaf <jwan.khalaf@outlook.com>
2019-04-01 14:49:49 +0100

    added two new components

--- src/components/home/App.js
+++ src/components/home/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router } from '@reach/router';
+import { LoginControls } from '../logincontrols/LoginControls';
+import { Authorisation } from '../authorisation/Authorisation';
 
 class App extends React.Component {
   render()
   {
     return (
       <div>
-        <h1 className="title">Hello from ReactJS</h1>
+        <h1 className="title">Hello from Bejebeje</h1>
+        <Router>
+          <LoginControls path="/" />
+          <Authorisation path="/callback" />
+        </Router>
       </div>
     );
   }
