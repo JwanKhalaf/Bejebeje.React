31f345ca42053f6de616e610eda426f027cbca17
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-05-06 14:35:20 +0100

    added a basic header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.scss";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="is-header">
+        <h1>Bejebeje</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/components/home/App.js
+++ src/components/home/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
-import LoginControls from "../logincontrols/LoginControls";
 import Authorisation from "../authorisation/Authorisation";
 import Artists from "../artists/Artists";
 
@@ -9,12 +8,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="title">Hello from Bejebeje</h1>
         <Router>
-          <LoginControls path="/" />
+          <Artists path="/" />
           <Authorisation path="/callback" />
         </Router>
-        <Artists />
       </div>
     );
   }
