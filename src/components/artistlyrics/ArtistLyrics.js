2b8c1f5c9b912c1ec1a9c1a2105f095379c5f538
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-05-07 19:04:50 +0100

    moved images to src so that I can import them, and also added a new artist lyrics component that lists the lyrics for chosen artist

--- src/components/artistlyrics/ArtistLyrics.js
+++ src/components/artistlyrics/ArtistLyrics.js
@@ -1,14 +1,60 @@
 import React from "react";
 import Header from "../header/Header";
+import { Link } from "@reach/router";
+import "./artistlyrics.scss";
 
 class ArtistLyrics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      lyrics: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5010/artists/" + this.props.artist + "/lyrics")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            lyrics: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
   render() {
-    return (
-      <>
-        <Header />
-        <h1>lyrics baby!</h1>
-      </>
-    );
+    const { error, isLoaded, lyrics: items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          <Header />
+          <ul className="is-lyrics-list">
+            {items.map(item => (
+              <li key={item.slug} className="is-lyric-list-item">
+                <Link to={item.slug + "/lyrics"}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      );
+    }
   }
 }
 
