c012d1d669646d0619623885bb2a10df61b9b72c
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-05-08 11:52:19 +0100

    added a facebook style pre-load animation

--- src/components/lyric/Lyric.js
+++ src/components/lyric/Lyric.js
@@ -44,7 +44,7 @@ class Lyric extends React.Component {
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      return <Header />;
     } else {
       return (
         <>
