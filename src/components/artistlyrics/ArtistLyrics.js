f8fab7b8c6d20c4726414a60213ad78c7ce4e67c
jwan <jwan.khalaf@outlook.com>
2019-05-11 10:49:31 +0100

    removed comments from fetch calls

--- src/components/artistlyrics/ArtistLyrics.js
+++ src/components/artistlyrics/ArtistLyrics.js
@@ -24,9 +24,6 @@ class ArtistLyrics extends React.Component {
             lyrics: result
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         error => {
           this.setState({
             isLoaded: true,
