a4c607df1a856ee2cfcade45e5ab009f12df9e17
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-07-17 22:01:45 +0100

    styled the buttons a bit and added cursor pointer

--- src/components/LoginControls/LoginControls.jsx
+++ src/components/LoginControls/LoginControls.jsx
@@ -1,17 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 import { AuthConsumer } from "../AuthProvider/AuthProvider";
+
+const LoginButton = styled.button`
+  background-color: green;
+  cursor: pointer;
+  padding: 5px 10px;
+  border: none;
+  color: #ffffff;
+  margin-right: 15px;
+  font-weight: bold;
+`;
+
+const LogoutButton = styled.button`
+  background-color: red;
+  cursor: pointer;
+  padding: 5px 10px;
+  border: none;
+  color: #ffffff;
+  margin-right: 15px;
+  font-weight: bold;
+`;
 
 function LoginControls(props) {
   return (
     <AuthConsumer>
       {value => (
         <>
-          <button id="login-button" onClick={value.login}>
+          <LoginButton id="login-button" onClick={value.login}>
             Login
-          </button>
-          <button id="logout-button" onClick={value.logout}>
+          </LoginButton>
+          <LogoutButton id="logout-button" onClick={value.logout}>
             Logout
-          </button>
+          </LogoutButton>
         </>
       )}
     </AuthConsumer>
